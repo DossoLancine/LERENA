14bfab604aaca9fe3d459e9923cb1e2b4d0c987e
Dosso Lancine <dossoefficacite@gmail.com>
2026-09-20 17:34:49 +0100

    fix: replace cartodb tiles with esri light gray to fix api key required watermarks

--- src/components/ExploreMap.tsx
+++ src/components/ExploreMap.tsx
@@ -80,8 +80,8 @@ export default function ExploreMap({ orgs, userLocation }: { orgs: any[], userLo
         zoomControl={false}
       >
         <TileLayer
-          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+          url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}"
+          attribution='Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ'
         />
         
         <LocationMarker location={userLocation} />
--- src/components/LocationPickerMap.tsx
+++ src/components/LocationPickerMap.tsx
@@ -98,8 +98,8 @@ export default function LocationPickerMap({
           className="w-full h-full"
         >
           <TileLayer
-            url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+            url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}"
+            attribution='Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ'
           />
           <MapController center={position} />
           
